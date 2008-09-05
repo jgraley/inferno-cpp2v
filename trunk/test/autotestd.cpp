dad87039a9b4566dcbe1602c40614ae570287aa6
jgraley <jgraley@365e59ee-f11f-0410-b96b-d703f2162f49>
2008-09-05 00:33:22 +0000

    fix conflict

--- trunk/test/autotestd.cpp
+++ trunk/test/autotestd.cpp
@@ -6,48 +6,48 @@
 #include <stdlib.h>
 
 
-int do_failable_test()
+const char *do_failable_test()
 {
     int rc = system("make");
     if( rc != 0 )
     {
         printf("Build failed\n");
-        return 1;
+        return "build failed";
     }
     
     printf("Build succeeded\n");
     
-    assert( chdir("test") == 0 );    
-    rc = system("make test");    
-    assert( chdir("..") == 0 );
- 
+    assert( chdir("test")==0 );
+    rc = system("make test");  
+    assert( chdir("..")==0 );   
+    
     if( rc != 0 )
     {
         printf("Tests failed\n");
-        return 2;
+        return "tests failed";
     }
     
-    printf("Tests passed\n");
-    
-    return 0;
+    printf("Tests passed\n");    
+    return "tests passed";
 }
 
 void do_test( int revision )
 {
-    printf("BEGIN TEST REVISION %d\n", revision );
-    
+    printf("BEGIN TEST REVISION %d\n", revision );    
     
     // Check out the requested revision (head might have moved on in the 
     // meantime)
-    char svn_up[100];
-    sprintf( svn_up, "svn update --revision %d\n", revision );
-    assert( system(svn_up)==0 );
+    char s[100];
+    sprintf( s, "svn update --revision %d\n", revision );
+    assert( system(s)==0 );
     
-    int rc = do_failable_test();    
-    
-    printf("END TEST\n\n");
+    const char *rs = do_failable_test();    
+        
+    printf( "@COMMIT: rev %d: %s\n", revision, rs );
+        
+    printf("END TEST REVISION %d\n", revision );
 }
- 
+
 // Must be invoked in reverse order ie last lable first because
 // we insert null terminators
 char *locate( char *buf, const char *label )
@@ -62,27 +62,19 @@ char *locate( char *buf, const char *label )
 
 void handle_message( char *buf )
 {   
-    printf("Got message %d bytes\n", strlen(buf) );
-
     // Detect and null-terminate the fields corresponding to the specifed labels
     char *mp = locate( buf, "Modified Paths:\n--------------\n" );
     char *lm = locate( buf, "Log Message:\n-----------\n" );
     char *date = locate( buf, "Date:     " );
     char *auth = locate( buf, "Author:   " );
     char *rev = locate( buf, "Revision: " );
-    if( !mp || !lm || !date || !auth || !rev )    
-    {
-        printf("Could not locate labels %p %p %p %p %p\nMaybe not a checkin message?\n\n", mp, lm, date, auth, rev ); 
+    if( !mp || !lm || !date || !auth || !rev )
         return;
-    }
     
     // Only proceed if a test was asked for in the log message. This is important
     // to prevent loops where test results trigger more tests.
     if( !strstr( lm, "test please" ) )
-    {
-        printf("No test requested\n\n");
         return;
-    }
         
     do_test( atoi( rev ) );    
 }
@@ -109,11 +101,8 @@ void handle_char( char c )
 
 int main( int argc, char **argv )
 {
-    // Should be run from trunk/test. Traverse a bit to confirm.
-    // Only test the trunk (branches are for "blue sky" stuff,
-    // wheras the trunk must work.
     assert( chdir("../../../inferno-synth/trunk")==0 );
-    
+
     // We must correctly handle >1 message in a row
     // as well as waiting when there are no meessags.
     // read() seems to return with 0 after the first 
