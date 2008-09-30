1ec7b6b85cb1eec533c3119abc9f857bce90c5ba
jgraley <jgraley@365e59ee-f11f-0410-b96b-d703f2162f49>
2008-09-30 10:31:09 +0000

    Separate parse and render classes

--- code/inferno.cpp
+++ code/inferno.cpp
@@ -16,160 +16,194 @@
 
 #define INFERNO_TRIPLE "arm-linux" 
 
-RCPtr<Program> program;
-RCPtr< Sequence<ProgramElement> > curseq;
-
-bool isf=false;
-
-/*
-Note: the default implementation of ActOnStartOfFunctionDef() appears in
-MinimalAction and can cause spurious ActOnDeclarator() calls if we always
-call through. Therefore we don't and instead just call explicitly implemented
-functions in MinimalAction where required.
-*/
-
-class InfernoAction : public clang::MinimalAction
+class Parse
 {
 public:
-    InfernoAction(clang::IdentifierTable &IT) : MinimalAction(IT)
+    void operator()( RCPtr<Program> program, std::string infile )
     {
-    }
- 
- private:   
-    Type *ConvertType( const clang::DeclSpec &DS )
-    {
-        switch( DS.getTypeSpecType() )
+        clang::FileManager fm; 
+        clang::TextDiagnosticPrinter diag_printer;
+        clang::Diagnostic diags( &diag_printer ); 
+        clang::LangOptions opts;
+        clang::TargetInfo* ptarget = clang::TargetInfo::CreateTargetInfo(INFERNO_TRIPLE);
+        assert(ptarget);
+        clang::SourceManager sm;
+        clang::HeaderSearch headers( fm );
+        
+        clang::Preprocessor pp( diags, opts, *ptarget, sm, headers );
+        
+        const clang::FileEntry *file = fm.getFile(infile);
+        if (file) 
+            sm.createMainFileID(file, clang::SourceLocation());
+        if (sm.getMainFileID() == 0) 
         {
-            case clang::DeclSpec::TST_int:
-                return new Int();
-                break;
-            case clang::DeclSpec::TST_char:
-                return new Char();
-                break;
-            default:
-                assert(0);
-                break;
+            fprintf(stderr, "Error reading '%s'!\n",infile.c_str());
+            exit(1);
         }
+        pp.EnterMainSourceFile();
+    
+        clang::IdentifierTable it( opts );                 
+        InfernoAction actions( program, it );                 
+        
+        clang::Parser parser( pp, actions );
+        
+        parser.ParseTranslationUnit();        
     }
     
-    Identifier *ConvertIdentifier( const clang::IdentifierInfo *ID )
+private:
+    class InfernoAction : public clang::MinimalAction
     {
-        Identifier *i = new Identifier();
-        i->assign( ID->getName() );
-        return i;
-    }
+    public:
+        InfernoAction(RCPtr<Program> p, clang::IdentifierTable &IT) : 
+            MinimalAction(IT),
+            program(p),
+            curseq(p)
+        {
+        }
+     
+        ~InfernoAction()
+        {
+            assert( &*curseq==&*program ); // TODO operator== in RCPtr<> 
+        }
+     
+     private:   
+        RCPtr<Program> program;
+        RCPtr< Sequence<ProgramElement> > curseq;
     
-    virtual DeclTy *ActOnDeclarator( clang::Scope *S, clang::Declarator &D, DeclTy *LastInGroup )
-    {
-        RCPtr<VariableDeclarator> p = new VariableDeclarator;
-        curseq->push_back(p);
-        p->storage_class = VariableDeclarator::STATIC;
-        p->type = ConvertType( D.getDeclSpec() );
-        p->identifier = ConvertIdentifier( D.getIdentifier() );        
-        //printf("cs %p\n", &*curseq );
-        //printf("id %s %p %p\n", p->identifier->c_str(), &*p->identifier, &*p );
-        (void)MinimalAction::ActOnDeclarator( S, D, LastInGroup );     
-        return 0;
-    }
+        Type *ConvertType( const clang::DeclSpec &DS )
+        {
+            switch( DS.getTypeSpecType() )
+            {
+                case clang::DeclSpec::TST_int:
+                    return new Int();
+                    break;
+                case clang::DeclSpec::TST_char:
+                    return new Char();
+                    break;
+                default:
+                    assert(0);
+                    break;
+            }
+        }
+        
+        Identifier *ConvertIdentifier( const clang::IdentifierInfo *ID )
+        { 
+            Identifier *i = new Identifier();
+            i->assign( ID->getName() );
+            return i;
+        }
+        
+        virtual DeclTy *ActOnDeclarator( clang::Scope *S, clang::Declarator &D, DeclTy *LastInGroup )
+        {
+            RCPtr<VariableDeclarator> p = new VariableDeclarator;
+            curseq->push_back(p);
+            p->storage_class = VariableDeclarator::STATIC;
+            p->type = ConvertType( D.getDeclSpec() );
+            p->identifier = ConvertIdentifier( D.getIdentifier() );        
+            //printf("cs %p\n", &*curseq );
+            //printf("id %s %p %p\n", p->identifier->c_str(), &*p->identifier, &*p );
+            (void)MinimalAction::ActOnDeclarator( S, D, LastInGroup );     
+            return 0;
+        }
+        
+        /*
+        Note: the default implementation of ActOnStartOfFunctionDef() appears in
+        MinimalAction and can cause spurious ActOnDeclarator() calls if we always
+        call through. Therefore we don't and instead just call explicitly implemented
+        functions in MinimalAction where required.
+        */
+        virtual DeclTy *ActOnStartOfFunctionDef(clang::Scope *FnBodyScope, clang::Declarator &D) 
+        {
+            RCPtr<FunctionDeclarator> p = new FunctionDeclarator;
+            curseq->push_back(p);
+            p->return_type = ConvertType( D.getDeclSpec() );
+            p->body = new Sequence<ProgramElement>;
+            p->identifier = ConvertIdentifier( D.getIdentifier() );  
     
-    virtual DeclTy *ActOnStartOfFunctionDef(clang::Scope *FnBodyScope, clang::Declarator &D) 
-    {
-        RCPtr<FunctionDeclarator> p = new FunctionDeclarator;
-        curseq->push_back(p);
-        p->return_type = ConvertType( D.getDeclSpec() );
-        p->body = new Sequence<ProgramElement>;
-        p->identifier = ConvertIdentifier( D.getIdentifier() );  
+            curseq = p->body;
+            return 0;     
+        }
+        
+        virtual DeclTy *ActOnFinishFunctionBody(DeclTy *Decl, StmtTy *Body) 
+        {
+            curseq = program;
+            return 0;
+        }    
+    };
+};    
+    
 
-        curseq = p->body;
-        return 0;     
-    }
-    
-    virtual DeclTy *ActOnFinishFunctionBody(DeclTy *Decl, StmtTy *Body) 
+
+class Render
+{
+public:
+    std::string operator()( RCPtr<Program> program )       
     {
-        curseq = program;
-        return 0;
+        return RenderSPE( program );
     }
 
+private:
+    std::string RenderType( RCPtr<Type> type )
+    {
+        if( DynamicCast< Int >(type) )
+            return "int";
+        else if( DynamicCast< Char >(type) )
+            return "char";
+    }
+    
+    std::string RenderSPE( RCPtr< Sequence<ProgramElement> > spe )
+    {
+        std::string s;
+        for( int i=0; i<spe->size(); i++ )
+        {
+            RCPtr<ProgramElement> pe = (*spe)[i];
+            if( RCPtr<VariableDeclarator> vd = DynamicCast< VariableDeclarator >(pe) )
+            {
+                s += RenderType(vd->type)+" "+(*vd->identifier)+";\n";
+            }
+            else if( RCPtr<FunctionDeclarator> fd = DynamicCast< FunctionDeclarator >(pe) )
+            {
+                s += RenderType(fd->return_type)+" "+(*fd->identifier)+"()\n{\n" + RenderSPE(fd->body) + "}\n";
+            }
+        }
+        return s;
+    }
 };
 
-
-std::string RenderType( RCPtr<const Type> type )
+static std::string infile;
+void read_args( int argc, char *argv[] )
 {
-    if( DynamicCast< const Int >(type) )
-        return "int";
-    else if( DynamicCast< const Char >(type) )
-        return "char";
-}
-
-std::string Render( RCPtr< Sequence<ProgramElement> > program )
-{
-    std::string s;
-    for( int i=0; i<program->size(); i++ )
-    {
-        RCPtr<ProgramElement> pe = (*program)[i];
-        if( RCPtr<VariableDeclarator> vd = DynamicCast< VariableDeclarator >(pe) )
-        {
-            //printf("id %s %p %p\n", vd->identifier->c_str(), &*vd->identifier, &*vd );
-            s += RenderType(vd->type)+" "+(*vd->identifier)+";\n";
-        }
-        else if( RCPtr<FunctionDeclarator> fd = DynamicCast< FunctionDeclarator >(pe) )
-        {
-            s += RenderType(fd->return_type)+" "+(*fd->identifier)+"()\n{\n" + Render(fd->body) + "}\n";
-        }
-    }
-    return s;
-}
-
-int main( int argc, char *argv[] )
-{
-    RCTarget::Start();
-
-    std::string infile;
     int i=1;
     while( i<argc )
     {
         if( argv[i]==std::string("-i") && argc>i+1 )
+        {
             infile = argv[++i];
+        }
         else 
         {
             fprintf(stderr, "Usage: inferno -i <infile>\n");
             exit(1);
         }
         i++;    
-    }
+    }    
+}
 
-    clang::FileManager fm; 
-    clang::TextDiagnosticPrinter diag_printer;
-    clang::Diagnostic diags( &diag_printer ); 
-    clang::LangOptions opts;
-    clang::TargetInfo* ptarget = clang::TargetInfo::CreateTargetInfo(INFERNO_TRIPLE);
-    assert(ptarget);
-    clang::SourceManager sm;
-    clang::HeaderSearch headers( fm );
-    
-    clang::Preprocessor pp( diags, opts, *ptarget, sm, headers );
-    
-    const clang::FileEntry *file = fm.getFile(infile);
-    if (file) 
-        sm.createMainFileID(file, clang::SourceLocation());
-    if (sm.getMainFileID() == 0) 
-    {
-        fprintf(stderr, "Error reading '%s'!\n",infile.c_str());
-        return true;
-    }
-    pp.EnterMainSourceFile();
 
-    clang::IdentifierTable it( opts );                 
-    InfernoAction actions( it );                 
+int main( int argc, char *argv[] )
+{
+    RCTarget::Start();
+
+    read_args( argc, argv );
+
+    RCPtr<Program> program = new Program();  
+
+    Parse p;    
+    p( program, infile );
     
-    clang::Parser parser( pp, actions );
+    Render r;
+    std::string ss = r( program );
     
-    program = new Program();  
-    curseq = program;
-    parser.ParseTranslationUnit();
-    assert( &*curseq==&*program );
-    
-    std::string ss = Render( program );
     printf( "%s", ss.c_str() );   
         
     RCTarget::Finished();
