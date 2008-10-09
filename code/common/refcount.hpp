cb2c65234a5e4528432cd1d6abbed3f0e6fe09bb
jgraley <jgraley@365e59ee-f11f-0410-b96b-d703f2162f49>
2008-10-09 19:08:10 +0000

    Remove old RCPtr implementation

--- code/common/refcount.hpp
+++ code/common/refcount.hpp
@@ -46,8 +46,6 @@ private:
     unsigned valid;    
 };
 
-#if 1
-
 #include <boost/shared_ptr.hpp>
 
 template<class TARGET>
@@ -81,126 +79,6 @@ public:
     
 };
 
-#else
-
-template<class TARGET>
-class RCPtr
-{
-public:
-    inline RCPtr() :
-        ptr( 0 )
-    {
-    }
-    
-    inline RCPtr( const RCPtr<TARGET> &o )
-    {
-        ptr = o.ptr;
-        Inc();
-    }
-
-    inline RCPtr( TARGET *t )
-    {
-        ptr = t;
-        Inc();
-    }
-
-    template<class OTHER_TARGET>
-    inline RCPtr( const RCPtr<OTHER_TARGET> &o )
-    { 
-        ptr = o.ptr;
-        Inc();
-    }
-  
-    template<class OTHER_TARGET>
-    inline RCPtr &operator =( const RCPtr<OTHER_TARGET> &o )
-    {
-        o.Inc(); 
-        Dec();
-        ptr = o.ptr;
-        return *this;
-    }
-    
-    inline RCPtr &operator =( const RCPtr<TARGET> &o )
-    {
-        o.Inc(); 
-        Dec();
-        ptr = o.ptr;
-        return *this;
-    }
-    
-    template<class OTHER_TARGET>
-    void ff( const RCPtr<OTHER_TARGET> &o )
-    {
-    }
-    
-    inline RCPtr &operator =( TARGET *t )
-    {
-        Dec();
-        ptr = t;
-        Inc();
-        return *this;
-    }
-    
-    inline ~RCPtr()
-    {
-        Dec();
-    }
-    
-    inline TARGET *operator ->()
-    {
-        assert( ptr && "dereferencing null RCPtr");
-        return ptr;
-    }
-
-    inline operator TARGET *()
-    {
-        return ptr;
-    }
-
-    inline operator bool()
-    {
-        return (bool)ptr;
-    }
-
-    inline TARGET &operator *()
-    {
-        return *ptr;
-    }
-
-    TARGET *ptr; // not private due to problems friending across template instantations
-    
-    inline void Inc() const 
-    {
-        if( ptr && !RCTarget::finished )
-        {        
-            ((RCTarget*)ptr)->ref_count++;
-            //printf("%p++%d\n", (RCTarget*)ptr, ((RCTarget*)ptr)->ref_count);
-        }
-    }
-    inline void Dec() const 
-    {
-        if( ptr && !RCTarget::finished )
-        {
-            assert( ((RCTarget *)ptr)->ref_count != 0 && "ref count out of sync");
-            ((RCTarget *)ptr)->ref_count--;
-            //printf("%p--%d\n", (RCTarget*)ptr, ((RCTarget*)ptr)->ref_count);
-            if( ((RCTarget *)ptr)->ref_count==0 )
-                delete ptr;
-        }
-    }
-
-    // A version of dynamic_cast for RCPtr. Conveniently, you only need to
-    // explicitly specify CANDIDATE, so the syntax is identical to 
-    // dynamic_cast.
-    template<class BASE >
-    static RCPtr Specialise( RCPtr<BASE> p )
-    {
-        assert( p && "Input to Specialise<>() must be non-NULL"); // since we return NULL when the cast isn't possible
-        return RCPtr<TARGET>( dynamic_cast<TARGET *>(p.ptr) );
-    }
-};
-
-#endif
 
 class RCHold
 {
