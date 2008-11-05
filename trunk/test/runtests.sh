c4d4179562c5943df6495f2d9f1f3aa8dd5ee42c
jgraley <jgraley@365e59ee-f11f-0410-b96b-d703f2162f49>
2008-11-05 01:23:42 +0000

    add test script

---
+++ trunk/test/runtests.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+clang_tests=../../../llvm-2.3/tools/clang/test
+inferno=../code/inferno.exe
+resfile=results.csv
+
+rm -f $resfile
+for infile in $clang_tests/CodeGen/*
+do
+ fb=`basename $infile`
+ outfile=results/$fb
+
+ $inferno -i $infile > $outfile
+ ires=$?
+
+ gccres=0
+ if test $ires -eq 0
+ then
+  g++ -c $outfile
+  gccres=$?
+ fi
+
+ echo $fb ", " $ires ", " $gccres >> $resfile
+done
