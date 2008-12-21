4c42564e84d1fb4104de1448fdc48261bc0b968b
jgraley <jgraley@365e59ee-f11f-0410-b96b-d703f2162f49>
2008-12-21 19:46:30 +0000

    inferno_minimal_action->identifier_tracker and make it a helper class, not base class

--- code/parse/identifier_tracker.hpp
+++ code/parse/identifier_tracker.hpp
@@ -1,6 +1,6 @@
 
-#ifndef INFERNO_MINIMAL_ACTION
-#define INFERNO_MINIMAL_ACTION
+#ifndef IDENTIFIER_TRACKER
+#define IDENTIFIER_TRACKER
 
 #include "clang/Parse/Action.h"
 #include "common/refcount.hpp"
@@ -17,29 +17,27 @@
   // Lex.
   class Token;
 
-/// InfernoMinimalAction - Minimal actions are used by light-weight clients of the
+/// IdentifierTracker - Minimal actions are used by light-weight clients of the
 /// parser that do not need name resolution or significant semantic analysis to
 /// be performed.  The actions implemented here are in the form of unresolved
 /// identifiers.  By using a simpler interface than the SemanticAction class,
 /// the parser doesn't have to build complex data structures and thus runs more
 /// quickly.
-class InfernoMinimalAction : public clang::Action {
+class IdentifierTracker
+{
   /// Translation Unit clang::Scope - useful to Objective-C actions that need
   /// to lookup file scope declarations in the "ordinary" C decl namespace.
   /// For example, user-defined classes, built-in "id" type, etc.
   clang::Scope *TUScope;
   clang::IdentifierTable &Idents;
-public:
-  InfernoMinimalAction(clang::IdentifierTable &IT) : Idents(IT) {}
-  
-  // Just added to get it to build TODO implement properly
-  virtual bool isCurrentClassName(const clang::IdentifierInfo& II, clang::Scope *S, const clang::CXXScopeSpec *SS) { return false;  }
 
+public:
+  IdentifierTracker(clang::IdentifierTable &IT) : Idents(IT) {}
   
   /// ActOnDeclarator - If this is a typedef declarator, we modify the
   /// clang::IdentifierInfo::FETokenInfo field to keep track of this fact, until S is
   /// popped.
-  virtual DeclTy *ActOnDeclarator(clang::Scope *S, clang::Declarator &D, DeclTy *LastInGroup, shared_ptr<Node> rcp);
+  virtual clang::Action::DeclTy *ActOnDeclarator(clang::Scope *S, clang::Declarator &D, clang::Action::DeclTy *LastInGroup, shared_ptr<Node> rcp);
   
   void AddNakedIdentifier(clang::Scope *S, clang::IdentifierInfo *II, shared_ptr<Node> rcp);  
   
@@ -50,10 +48,10 @@ public:
   
   // Extract the shared_ptr for the identifier. Where the identifier is differently declared
   // in nested scopes, we get the one that applies currently (which is the innermost one)  
-  shared_ptr<Node> InfernoMinimalAction::GetCurrentIdentifierRCPtr( const clang::IdentifierInfo &II );                                         
+  shared_ptr<Node> IdentifierTracker::GetCurrentIdentifierRCPtr( const clang::IdentifierInfo &II );                                         
   
   // Version that just results NULL if identifier has not been added yet
-  shared_ptr<Node> InfernoMinimalAction::TryGetCurrentIdentifierRCPtr( const clang::IdentifierInfo &II );                                         
+  shared_ptr<Node> IdentifierTracker::TryGetCurrentIdentifierRCPtr( const clang::IdentifierInfo &II );                                         
 };
 
 #endif
