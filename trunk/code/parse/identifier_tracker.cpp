643785aac7bd4b9df33abae70504150141df7034
jgraley <jgraley@365e59ee-f11f-0410-b96b-d703f2162f49>
2008-12-21 20:15:58 +0000

    cleaning up IdentifierTracker

--- trunk/code/parse/identifier_tracker.cpp
+++ trunk/code/parse/identifier_tracker.cpp
@@ -18,47 +18,19 @@ struct TypeNameInfo {
   }
 };
 
-void IdentifierTracker::ActOnTranslationUnitScope(clang::SourceLocation Loc, clang::Scope *S) 
-{
-  TUScope = S;
-  clang::IdentifierInfo *II;
-  TypeNameInfo *TI;
-  
-  // recognize the ObjC built-in type identifiers.
-  /* JSG I don't like doing this here TODO if supporting objective-C, do 
-     this somewhere else (and make sure the actions get invoked)
-  II = &Idents.get("id");
-  TI = new TypeNameInfo(1, II->getFETokenInfo<TypeNameInfo>());
-  II->setFETokenInfo(TI);
-  II = &Idents.get("SEL");
-  TI = new TypeNameInfo(1, II->getFETokenInfo<TypeNameInfo>());
-  II->setFETokenInfo(TI);
-  II = &Idents.get("Class");
-  TI = new TypeNameInfo(1, II->getFETokenInfo<TypeNameInfo>());
-  II->setFETokenInfo(TI);
-  II = &Idents.get("Protocol");
-  TI = new TypeNameInfo(1, II->getFETokenInfo<TypeNameInfo>());
-  II->setFETokenInfo(TI);
-  */
-}
-
-
-/// ActOnDeclarator - If this is a typedef declarator, we modify the
-/// clang::IdentifierInfo::FETokenInfo field to keep track of this fact, until S is
-/// popped.
-clang::Action::DeclTy *
-IdentifierTracker::ActOnDeclarator(clang::Scope *S, clang::Declarator &D, clang::Action::DeclTy *LastInGroup, shared_ptr<Node> rcp) 
+void IdentifierTracker::Add(clang::Scope *S, clang::Declarator &D, shared_ptr<Node> rcp) 
 {
   clang::IdentifierInfo *II = D.getIdentifier();
   TRACE("pushing identifier \"%s\"\n", D.getIdentifier()->getName() );
-  // If there is no identifier associated with this declarator, bail out.
-  if (II == 0) return 0;
   
-  AddNakedIdentifier(S, II, rcp); 
-  return 0;
+  // If there is no identifier associated with this declarator, bail out.
+  if (II == 0) 
+      return;
+  
+  Add(S, II, rcp);
 }
 
-void IdentifierTracker::AddNakedIdentifier(clang::Scope *S, clang::IdentifierInfo *II, shared_ptr<Node> rcp) 
+void IdentifierTracker::Add(clang::Scope *S, clang::IdentifierInfo *II, shared_ptr<Node> rcp) 
 {
   TypeNameInfo *weCurrentlyHaveTypeInfo = II->getFETokenInfo<TypeNameInfo>();
   TypeNameInfo *TI = new TypeNameInfo(weCurrentlyHaveTypeInfo, rcp);
@@ -70,13 +42,16 @@ void IdentifierTracker::AddNakedIdentifier(clang::Scope *S, clang::IdentifierInf
 
 /// ActOnPopScope - When a scope is popped, if any typedefs are now
 /// out-of-scope, they are removed from the clang::IdentifierInfo::FETokenInfo field.
-void IdentifierTracker::ActOnPopScope(clang::SourceLocation Loc, clang::Scope *S) {
+void IdentifierTracker::PopScope(clang::Scope *S) 
+{
   for (clang::Scope::decl_iterator I = S->decl_begin(), E = S->decl_end();
-       I != E; ++I) {
+       I != E; ++I) 
+  {
     clang::IdentifierInfo &II = *static_cast<clang::IdentifierInfo*>(*I);
     TypeNameInfo *TI = II.getFETokenInfo<TypeNameInfo>();
     ASSERT(TI && "This decl didn't get pushed??");
-    if (TI) {
+    if (TI) 
+    {
       TypeNameInfo *Next = TI->Prev;
       delete TI;
       
