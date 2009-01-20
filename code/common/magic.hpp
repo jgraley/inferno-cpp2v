cce2410cb1c3d434baa9d83e3ec5066adc84948a
jgraley <jgraley@365e59ee-f11f-0410-b96b-d703f2162f49>
2009-01-20 20:28:35 +0000

    Add magic number checks so we can spot memory corruption in the tree sooner

---
+++ code/common/magic.hpp
@@ -0,0 +1,22 @@
+#ifndef MAGIC_HPP
+#define MAGIC_HPP
+
+// Magic number checks for memory corruption
+
+class Magic
+{
+    unsigned magic;
+public:
+    Magic() :
+        magic(0x12343210)
+    {    
+    }    
+
+    ~Magic()
+    {
+        ASSERT(magic==0x12343210 && "magic number check failed");    
+        magic = 0xde1e7ed;
+    }    
+};
+
+#endif
