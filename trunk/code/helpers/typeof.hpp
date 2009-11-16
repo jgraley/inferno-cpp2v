559068519285d35faa39bcb360a6ed41f12837f0
jgraley <jgraley@365e59ee-f11f-0410-b96b-d703f2162f49>
2009-11-16 21:16:16 +0000

    added some more functionality to TypeOf, still not correct but we're passing tests now

--- trunk/code/helpers/typeof.hpp
+++ trunk/code/helpers/typeof.hpp
@@ -16,7 +16,10 @@ public:
     }
     
     shared_ptr<Type> Get( shared_ptr<Expression> o );
-    shared_ptr<Type> Get( shared_ptr<Operator> op, Sequence<Type> &optypes );
+    shared_ptr<Type> Get( shared_ptr<Operator> op, Sequence<Type> optypes );
+    shared_ptr<Type> GetStandard( Sequence<Type> &optypes );
+    shared_ptr<Type> GetStandard( Sequence<Numeric> &optypes );
+    shared_ptr<Type> GetSpecial( shared_ptr<Operator> op, Sequence<Type> &optypes );
     
     // Is this call really a constructor call? If so return the object being
     // constructed. Otherwise, return NULL
