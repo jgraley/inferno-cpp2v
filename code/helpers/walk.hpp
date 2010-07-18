e7f8db81028ab45dcc7d787770d4d0bec107d281
jgraley <jgraley@365e59ee-f11f-0410-b96b-d703f2162f49>
2010-07-18 15:48:47 +0000

    tidying up walk

--- code/helpers/walk.hpp
+++ code/helpers/walk.hpp
@@ -7,19 +7,35 @@
 class Walk : public ContainerInterface
 {
 public:
-
 	class iterator : public ContainerInterface::iterator_interface
 	{
-	    struct Frame
+	public:
+	    typedef forward_iterator_tag iterator_category;
+		typedef TreePtrInterface value_type;
+		typedef int difference_type;
+		typedef const value_type *pointer;
+		typedef const value_type &reference;
+
+		iterator( const iterator & other );
+		virtual shared_ptr<ContainerInterface::iterator_interface> Clone() const;
+		virtual iterator &operator++();
+		virtual reference operator*() const;
+		virtual pointer operator->() const;
+		virtual bool operator==( const ContainerInterface::iterator_interface &ib ) const;
+		virtual void Overwrite( pointer v ) const;
+        virtual const bool IsOrdered() const;
+	    int Depth() const;
+	    operator string() const;
+	    void AdvanceOver();
+	    void AdvanceInto();
+	private:
+        struct Frame
 	    {
 	        vector< ContainerInterface::iterator > children;
 	        int index;
 	    };
 
-	    shared_ptr< TreePtr<Node> > root;
-	    TreePtr<Node> restrictor;
-	    stack< Frame > state;
-
+	    iterator( TreePtr<Node> root, TreePtr<Node> restrictor );
 	    bool IsAtEndOfCollection() const;
 	    void BypassInvalid();
 	    void Push( TreePtr<Node> n );
@@ -30,86 +46,23 @@ public:
 	    TreePtr<Node> Get() const;
 	    void Set( TreePtr<Node> n );
 
-	public:
-	    iterator( TreePtr<Node> root=TreePtr<Node>(), // NULL root gets us a walk stuck on Done()
-	              TreePtr<Node> restrictor=TreePtr<Node>() );
-	    iterator( const iterator & other );
-	    int Depth() const;
-	    operator string() const;
-	    void AdvanceOver();
-	    void AdvanceInto();
+	    shared_ptr< TreePtr<Node> > root;
+	    TreePtr<Node> restrictor;
+	    stack< Frame > state;
 
-	    typedef forward_iterator_tag iterator_category;
-		typedef TreePtrInterface value_type;
-		typedef int difference_type;
-		typedef const value_type *pointer;
-		typedef const value_type &reference;
-
-		virtual shared_ptr<ContainerInterface::iterator_interface> Clone() const
-		{
-			shared_ptr<iterator> ni( new iterator(*this) );
-			return ni;
-		}
-
-		virtual iterator &operator++()
-		{
-			AdvanceInto();
-			return *this;
-		}
-
-		virtual reference operator*() const
-		{
-			return *GetIterator();
-		}
-
-		virtual pointer operator->() const
-		{
-			return &*GetIterator();
-		}
-
-		virtual bool operator==( const ContainerInterface::iterator_interface &ib ) const
-		{
-			const iterator *pi = dynamic_cast<const iterator *>(&ib);
-			ASSERT(pi)("Comparing walking iterator with something else ")(ib);
-			if( pi->Done() || Done() )
-				return pi->Done() && Done();
-			return pi->Get() == Get();
-		}
-
-		virtual void Overwrite( pointer v ) const
-		{
-			GetIterator().Overwrite( v );
-		}
-
-		virtual const bool IsOrdered() const
-		{
-			return true; // walk walks tree in order generally
-		}
-	};
+	    friend class Walk;
+    };
 
 	Walk( TreePtr<Node> r,
-			       TreePtr<Node> res = TreePtr<Node>() ) :
-		root(r),
-		restrictor(res),
-		my_begin( iterator( root, restrictor ) ),
-		my_end( iterator( TreePtr<Node>(), restrictor ) )
-    {
-    }
-	TreePtr<Node> root;
-    TreePtr<Node> restrictor;
-    iterator my_begin, my_end;
-    virtual const iterator &begin()
-    {
-    	my_begin = iterator( root, restrictor );
-    	return my_begin;
-    }
-    virtual const iterator &end()
-    {
-    	my_end = iterator( TreePtr<Node>(), restrictor );
-    	return my_end;
-    }
+		  TreePtr<Node> res = TreePtr<Node>() );
+    virtual const iterator &begin();
+    virtual const iterator &end();
     virtual void erase( ContainerInterface::iterator it ) { ASSERTFAIL("Cannot modify through walking container"); }
     virtual void clear() { ASSERTFAIL("Cannot modify through walking container"); }
+private:
+    TreePtr<Node> root;
+    TreePtr<Node> restrictor;
+    iterator my_begin, my_end;
 };
 
 
