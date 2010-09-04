b76d57c55976632cce80857017c971da6aaa5622
jgraley <jgraley@365e59ee-f11f-0410-b96b-d703f2162f49>
2010-09-04 16:22:48 +0000

    changes to comments

--- trunk/code/helpers/soft_patterns.hpp
+++ trunk/code/helpers/soft_patterns.hpp
@@ -11,11 +11,12 @@
 // sprintf( format, source->name )
 // You must key the source identifier to somehting in the search pattern (so it
 // will get substitued to the real Specific identifier found in the tree) and
-// you may substitute the SoftMakeIdentifier in the replace pattern if you need
+// you may couple the SoftMakeIdentifier in the replace pattern if you need
 // to specify it in more than one place. Note that SoftMakeIdentifier is stateless
 // and cannot therefore keep track of uniqueness - you'll get a new one each time
 // and must rely on a replace coupling to get multiple reference to the same
 // new identifier. Rule is: ONE of these per new identifier.
+// TODO rename to BuildIdentifier per the docs
 struct SoftMakeIdentifier : InstanceIdentifier, // TODO other kinds of identifier
                             RootedSearchReplace::SoftReplacePattern
 {
