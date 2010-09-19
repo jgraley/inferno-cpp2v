5004273d8daa57f8ddafed71af138131948d2714
jgraley <jgraley@365e59ee-f11f-0410-b96b-d703f2162f49>
2010-09-19 16:26:59 +0000

    moved GenerateStacks not new style slave

--- code/steps/generate_stacks.cpp
+++ code/steps/generate_stacks.cpp
@@ -23,13 +23,13 @@ struct ThisOne : SoftMakeIdentifier
 void GenerateStacks::operator()( TreePtr<Node> context, TreePtr<Node> *proot )
 {
 	TRACE();
-	MakeTreePtr<Instance> s_fi, r_fi, s_fi2, r_fi2, s_fi3, r_fi3;
+	MakeTreePtr<Instance> s_fi, r_fi;
 	MakeTreePtr<Subroutine> s_func;
 	MakeTreePtr< MatchAll<Initialiser> > s_and;
 	MakeTreePtr<Compound> s_top_comp, r_top_comp, s_ret_comp, sn_ret_comp, r_ret_comp;
 	MakeTreePtr< Star<Declaration> > s_top_decls, r_top_decls, s_ret_decls, sn_ret_decls, r_ret_decls;
 	MakeTreePtr< Star<Statement> > s_top_pre, r_top_pre, s_ret_pre, s_ret_post, sn_ret_pre, sn_ret_post, r_ret_pre, r_ret_post;
-	MakeTreePtr< Stuff<Statement> > cs_stuff, s_stuff, r_stuff, s_stuff3, r_stuff3;
+	MakeTreePtr< Stuff<Statement> > cs_stuff, s_stuff, r_stuff;
 	MakeTreePtr<Automatic> cs_instance, s_instance;
 	MakeTreePtr<Static> r_index, r_instance; // TODO Field
 	MakeTreePtr<Unsigned> r_index_type;
@@ -45,6 +45,8 @@ void GenerateStacks::operator()( TreePtr<Node> context, TreePtr<Node> *proot )
 	MakeTreePtr<SoftMakeIdentifier> r_index_identifier("%s_stack_index");
 	MakeTreePtr<ThisOne> r_identifier("%s_stack");
 
+
+
     // Master search - look for functions satisfying the construct limitation and get
 	s_fi->identifier = MakeTreePtr<InstanceIdentifier>();
 	s_fi->type = s_func;
@@ -57,7 +59,15 @@ void GenerateStacks::operator()( TreePtr<Node> context, TreePtr<Node> *proot )
     s_and->patterns = ( s_top_comp, cs_stuff );
 
 	// Master replace - insert index variable, inc and dec into function at top level
-	r_fi->initialiser = r_top_comp;
+	MakeTreePtr< Slave<Statement> > r_slave( r_stuff, ss_identifier, sr_sub );
+	MakeTreePtr< RootedSlave<Statement> > r_mid( r_top_comp, s_stuff, r_slave );
+
+#if HANDLE_EARLY_RETURNS
+	MakeTreePtr< Slave<Statement> > r_slave3( r_mid, s_and3, r_ret_comp );
+	r_fi->initialiser = r_slave3;
+#else
+	r_fi->initialiser = r_mid;
+#endif
 	// top-level decls
 	r_top_comp->members = ( r_top_decls, r_index );
 	r_index->type = r_index_type;
@@ -72,15 +82,12 @@ void GenerateStacks::operator()( TreePtr<Node> context, TreePtr<Node> *proot )
 	r_dec->operands = ( MakeTreePtr<InstanceIdentifier>() );
 
     // Slave search to find automatic variables within the function
-	s_fi2->identifier = MakeTreePtr<InstanceIdentifier>();
-	s_fi2->initialiser = s_stuff;
 	s_stuff->terminus = s_instance;
 	s_instance->identifier = s_identifier;
 	s_instance->initialiser = MakeTreePtr<Uninitialised>(); // can't handle initialisers!
 	s_instance->type = MakeTreePtr<Type>();
 
     // Slave replace to insert as a static array (TODO be a member of enclosing class)
-	r_fi2->initialiser = r_stuff;
 	r_instance->constancy = MakeTreePtr<NonConst>();
 	r_instance->initialiser = MakeTreePtr<Uninitialised>();
 	r_stuff->terminus = r_instance;
@@ -95,9 +102,6 @@ void GenerateStacks::operator()( TreePtr<Node> context, TreePtr<Node> *proot )
 
 #if HANDLE_EARLY_RETURNS
 	// Slave to find early returns in the function
-	s_fi3->identifier = MakeTreePtr<InstanceIdentifier>();
-	s_fi3->initialiser = s_stuff3;
-	s_stuff3->terminus = s_and3;
 	s_and3->patterns = ( s_ret_comp );
 	s_ret_comp->members = ( s_ret_decls );
 	s_ret_comp->statements = ( s_ret_pre, s_return, s_ret_post );
@@ -110,8 +114,6 @@ void GenerateStacks::operator()( TreePtr<Node> context, TreePtr<Node> *proot )
 	sn_ret_comp->statements = ( sn_ret_pre, sn_ret_dec, sn_return, sn_ret_post );
 
 	// Slave replace with a decrement of the stack index coming before the return
-	r_fi3->initialiser = r_stuff3;
-	r_stuff3->terminus = r_ret_comp;
 	r_ret_comp->members = ( r_ret_decls );
 	r_ret_dec->operands = ( MakeTreePtr<InstanceIdentifier>() );
 	r_ret_comp->statements = ( r_ret_pre, r_ret_dec, r_return, r_ret_post );
@@ -122,16 +124,13 @@ void GenerateStacks::operator()( TreePtr<Node> context, TreePtr<Node> *proot )
 		Coupling(( s_top_pre, r_top_pre )), // Couple pre-existing statements in the funciton's top level
 		Coupling(( s_stuff, r_stuff )), // Couple stuff between the function and the variable to be changed
 		Coupling(( s_fi, r_fi )), // Couple the original function
-		Coupling(( s_fi2, r_fi2 )), // Couple the function after master replace
 		Coupling(( s_instance->type, r_array->element )), // Couple the type of the auto variable into the element type of the array
 		Coupling(( s_identifier, r_identifier->source, ss_identifier )), // Couple the identifier of the auto variable for sub-slave and as source for array's name
 		Coupling(( r_identifier, sr_sub->operands[0] )), // Couple the name of the array into the base of the subscript
 
 #if HANDLE_EARLY_RETURNS
-		Coupling(( s_fi->identifier, r_index_identifier->source, s_fi2->identifier, s_fi3->identifier )),
+		Coupling(( s_fi->identifier, r_index_identifier->source )),
 		Coupling(( r_index_identifier, r_inc->operands[0], r_dec->operands[0], sr_sub->operands[1], r_ret_dec->operands[0], sn_ret_dec->operands[0] )),
-		Coupling(( s_fi3, r_fi3 )), // Couple the function for dec-before-return slave
-		Coupling(( s_stuff3, r_stuff3 )), // Couple stuff between function and compound containing return
 		Coupling(( s_ret_decls, r_ret_decls )), // Couple decls in compound containing return
 		Coupling(( s_ret_pre, r_ret_pre )), // Couple statements before return in compound
 		Coupling(( s_return, r_return )), // Couple the return statement
@@ -142,15 +141,5 @@ void GenerateStacks::operator()( TreePtr<Node> context, TreePtr<Node> *proot )
         Coupling(( r_index_identifier, r_inc->operands[0], r_dec->operands[0], sr_sub->index )) )); // Couple the name of the index variable into the index of the subscript and all the incs and decs
 #endif
 
-	vector<RootedSearchReplace *> vs;
-	SearchReplace slave( ss_identifier, sr_sub );
-	vs.push_back( &slave );
-	SearchReplace mid( s_fi2, r_fi2, sms, vs );
-	vector<RootedSearchReplace *> vs2;
-	vs2.push_back( &mid );
-#if HANDLE_EARLY_RETURNS
-	SearchReplace ret( s_fi3, r_fi3 );
-	vs2.push_back( &ret );
-#endif
-	SearchReplace( s_fi, r_fi, sms, vs2 )( context, proot );
+	SearchReplace( s_fi, r_fi, sms )( context, proot );
 }
