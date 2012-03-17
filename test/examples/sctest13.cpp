0d92c3505e4cb36fd1cedea13c28852647e8ee06
jgraley <jgraley@365e59ee-f11f-0410-b96b-d703f2162f49>
2012-03-17 20:31:48 +0000

    test for fors

---
+++ test/examples/sctest13.cpp
@@ -0,0 +1,45 @@
+
+// subordinate funciton with multiple parameters and return value
+
+#include <isystemc.h>
+
+class TopLevel : public sc_module
+{
+public:
+    int x, i;
+    float fi;
+    SC_CTOR(TopLevel)
+    {
+        SC_THREAD(T);        
+    }
+    void T()
+    {
+        x = 0;
+        for( i=0; i<4; i++ )
+            x += i;
+        for( i=0; i<4; i++ )
+            break;
+        for( i=0; i<4; i++ )
+            switch(0) { case 0: break; }
+//        for( i=0; i<4; i++ )
+//            continue;
+        for( ; i<4; i++ )
+            x += i;
+        for( i=0; i<x; i++ )
+            {};
+        for( i=0; i<4; i=i+1 )
+            x += i;
+        for( i=0; i<4; i++ )
+            i += 0;
+        for( i=0; i<4; i++ )
+            x += f();
+        cease(x);
+    }
+    int f()
+    {
+        return 3;
+    }
+};
+
+TopLevel top_level("top_level");
+
