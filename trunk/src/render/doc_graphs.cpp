b7c3f1db85d29db6d002870b0ea868e531f51e8b
jgraley <jgraley@365e59ee-f11f-0410-b96b-d703f2162f49>
2012-07-29 19:44:53 +0000

    sr doc gen stuff

--- trunk/src/render/doc_graphs.cpp
+++ trunk/src/render/doc_graphs.cpp
@@ -11,12 +11,22 @@
 
 using namespace CPPTree;
 
+/// Output a graph of the subtree at node, to a dot file with basename
+/// name and path supplied by user with -o (or local dir)
 static void Output( TreePtr<Node> root, string name )
 {
     string path = ReadArgs::outfile + name + string(".dot");
     TRACE("Generating graph ")(path)("\n");    
     Graph g( path );
     g( root ); 
+}
+
+/// Output a graph for search and replace documentation. For eg section 2.3a use major=2
+/// minor=3 and subsection="a". If no letter, leave subsection empty.
+static void OutputSR( TreePtr<Node> root, int major, int minor, string subsection, string tag )
+{
+    string name = SSPrintf( "srdoc_%d_%d%s_%s\n", major, minor, subsection.c_str(), tag.c_str() );
+    Output(root, name); 
 }
 
 void GenerateDocumentationGraphs()
