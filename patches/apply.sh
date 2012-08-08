b1903bce0f247b9401be5602fab207b8e2f4c07d
jgraley <jgraley@365e59ee-f11f-0410-b96b-d703f2162f49>
2012-08-08 21:17:17 +0000

    llvm patches for recent GCC changes

--- patches/apply.sh
+++ patches/apply.sh
@@ -9,3 +9,8 @@ rm ../llvm/tools/clang/lib/Driver/PlistDiagnostics.cpp
 # GCC4 puts string lib return values in const char *
 patch ../llvm/lib/System/Unix/Signals.inc gcc44_patch1.txt
 
+# GCC with Ubuntu 12.04 requires a #include for ptrdiff_t  
+patch ../llvm/include/llvm/ADT/ilist.h ptrdiff_patch1.txt
+patch ../llvm/include/llvm/ADT/SmallVector.h ptrdiff_patch2.txt
+patch ../llvm/include/llvm/Use.h ptrdiff_patch3.txt
+
