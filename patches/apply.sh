46ba4909f2e4b228603914874d6e31b5f7c17f04
jgraley <jgraley@365e59ee-f11f-0410-b96b-d703f2162f49>
2013-09-11 18:24:28 +0000

    new llvm patch for gcc 4.7

--- patches/apply.sh
+++ patches/apply.sh
@@ -6,11 +6,15 @@ rm ../llvm/tools/clang/lib/Driver/PlistDiagnostics.cpp
 # Inferno uses RTTI and wont link with non-RTTI
 ./kill_no_rtti.sh ../llvm/tools/clang/lib/*/Makefile
 
-# GCC4 puts string lib return values in const char *
+# GCC4.4 puts string lib return values in const char *
 patch ../llvm/lib/System/Unix/Signals.inc gcc44_patch1.txt
 
 # GCC with Ubuntu 12.04 requires a #include for ptrdiff_t  
 patch ../llvm/include/llvm/ADT/ilist.h ptrdiff_patch1.txt
 patch ../llvm/include/llvm/ADT/SmallVector.h ptrdiff_patch2.txt
 patch ../llvm/include/llvm/Use.h ptrdiff_patch3.txt
+
+# GCC4.7 fussy about something or other
+patch ../llvm/include/llvm/Support/CommandLine.h gcc47_patch1.txt
+
 
