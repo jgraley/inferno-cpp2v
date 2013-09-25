c40153d08133031d2ac4268c2d3000205be48428
jgraley <jgraley@365e59ee-f11f-0410-b96b-d703f2162f49>
2013-09-25 19:17:34 +0000

    improved concurrent test run

--- trunk/test/runtests.sh
+++ trunk/test/runtests.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Stop on error (errors in individual tests will be trapped)
+set -e
+
+# Decide where things should go
 clang_tests=llvm/tools/clang/test
 resfile=test/results.csv
+logdir=test/results
 
 # Usage:
 # runtests.sh - tests the known passing (regression) tests
@@ -9,44 +14,54 @@ resfile=test/results.csv
 # runtests.sh <file list> - tests all named files
 if test -z $1
 then
- infilelist="test/examples/*"
+    infilelist="test/examples/*"
 else 
- if test $1 == all
- then
-  infilelist="test/examples/* $clang_tests/CodeGen/*"
- else
-  infilelist=$*
- fi
+    if test $1 == all
+    then
+        infilelist="test/examples/* $clang_tests/CodeGen/*"
+    else
+        infilelist=$*
+    fi
 fi
 
+# Clear down output files/directories since we would not like out-of-date results
 rm -f $resfile
 mkdir -p results
 rm -f results/*
 
 echo testing files $infilelist
 
+# Prevent the pipe to tee from hiding the status code from test.sh
+set -o pipefail
+
+# Start all the tests in seperate processes to take advantage of mult-core host
 for infile in $infilelist
 do
- test/test.sh $infile $resfile -ap &
+    # Decide where to put the log file for this test run 
+    logfile=$logdir/`basename $infile`.log
+    # Start the test and pipe output to tee so we get it on the terminal and in the log file
+    test/test.sh $infile $resfile -ap 2>&1 | tee $logfile &
 done
 
+# Wait for all the tests to complete and count failures 
 failed=0
 for job in `jobs -p`
 do
+    # Wait for the numbered job, collect status code and increment the fail count if it was bad
+    # TODO could detect rejected tests (status code 2)
     wait $job || let "failed+=1"
 done
 
+# Report the overall results after all individual tests have completed
 echo
 echo ------------------------------------------
-
 echo -n "Tests Run: "
 date
 echo
-
 if test $failed -eq 0
 then
- echo "ALL TESTS PASSED"
+    echo "ALL TESTS PASSED"
 else
- echo "$failed TESTS FAILED"
+    echo "$failed TESTS FAILED"
 fi
 
