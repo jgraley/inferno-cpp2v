5b7109283f2c563d592b555065a53b4f1ef5a348
John <jgraley1@gmail.com>
2022-09-10 15:32:54 +0100

    close #626

--- src/common/serial.cpp
+++ src/common/serial.cpp
@@ -7,56 +7,7 @@
 #include <stdio.h>
 #include <iostream>
 
-//////////////////////////// LeakCheck ///////////////////////////////
-
-LeakCheck::~LeakCheck()
-{
-    instance_counts.at(origin).count--;
-    instance_counts.at(origin).destructs.insert( GetOrigin() );
-}
-
-
-void LeakCheck::Construct()
-{
-    if( instance_counts.count(origin)==0 )
-        instance_counts[origin] = {0};
-    instance_counts.at(origin).count++;
-}
-
-
-void LeakCheck::DumpCounts( int min )
-{
-    FTRACEC("LeakCheck:\n");
-    for( auto p : instance_counts )  
-        if( p.second.count >= min )
-            FTRACEC(p.first)
-                   (" %llu instances\n", p.second.count)
-                   ("destructs: ")(p.second.destructs)("\n");
-}
-
-#define TRY(SEQ, LEVEL) \
-    if( !ON_STACK( __builtin_frame_address(LEVEL) ) ) \
-        break; \
-    SEQ.push_back( __builtin_extract_return_addr(__builtin_return_address(LEVEL)) ); 
- 
-
-LeakCheck::Origin LeakCheck::GetOrigin()
-{
-    Origin o;
-    do 
-    {
-        TRY(o, 1)
-        TRY(o, 2)
-        TRY(o, 3)
-        TRY(o, 4)
-        TRY(o, 5)
-    } while(0);
-    return o;
-}
-
-#undef TRY
-
-map<LeakCheck::Origin, LeakCheck::Block> LeakCheck::instance_counts;
+#define USE_HOOK
 
 //////////////////////////// SerialNumber ///////////////////////////////
 
@@ -70,10 +21,56 @@ SerialNumber::SerialNumber()
 }    
 
 
+SerialNumber::SerialNumber( const SerialNumber &other ) :
+	SerialNumber() // Identity semantics: ignore "other"
+{      
+}
+
+
+inline SerialNumber &SerialNumber::operator=( const SerialNumber &other )
+{
+	// Identity semantics: ignore "other"
+	return *this;
+}
+
+
+bool SerialNumber::operator<( const SerialNumber &o ) const
+{
+	if( progress != o.progress )
+		return progress < o.progress;
+	else
+		return serial < o.serial;
+}
+
+
+SerialNumber::SNType SerialNumber::GetSerialNumber() const 
+{
+	return serial; // This is enough for uniqueness
+}
+
+
 string SerialNumber::GetSerialString() const
 {
     string pp = progress.GetPrefix();
     return SSPrintf("#%s-%lu", pp.c_str(), serial);    
+}
+
+
+void SerialNumber::SetHook(shared_ptr<Hook> h) const
+{
+	hook = h;
+}
+
+
+bool SerialNumber::HasHook() const
+{
+	return (bool)hook;
+}
+
+
+shared_ptr<SerialNumber::Hook> SerialNumber::GetHook() const
+{
+	return hook;
 }
 
 
@@ -125,28 +122,88 @@ SatelliteSerial::SatelliteSNType SatelliteSerial::DetermineSerialNumber( const S
     if( !mother )
         return 0;
     
-    SerialNumber::SNType sn = mother->GetSerialNumber();
-    MotherBlock &mother_block = blocks_by_mother_serial[sn];
-    if( mother_block.serial_by_satellite.count(satellite) == 0 )
+	shared_ptr<MotherBlock> mother_block;
+	if( mother->HasHook() )
+	{
+		mother_block = dynamic_pointer_cast<MotherBlock>( mother->GetHook() );
+		ASSERT( mother_block ); // Will be NULL if wrong type of block on hook
+	}
+	else
+	{
+		mother_block = make_shared<MotherBlock>();
+		mother->SetHook(mother_block); 
+	}
+
+    if( mother_block->serial_by_satellite.count(satellite) == 0 )
     {
-        int ns = mother_block.next_serial;
-        mother_block.next_serial++;
-        mother_block.serial_by_satellite[satellite] = ns;
+        int ns = mother_block->next_serial;
+        mother_block->next_serial++;
+        mother_block->serial_by_satellite[satellite] = ns;
         return ns;
     }
     else
     {
-        return mother_block.serial_by_satellite.at(satellite);
+        return mother_block->serial_by_satellite.at(satellite);
     }
 }
 
+//////////////////////////// LeakCheck ///////////////////////////////
 
-SatelliteSerial::BlocksByMotherSerial SatelliteSerial::blocks_by_mother_serial;    
+LeakCheck::~LeakCheck()
+{
+    instance_counts.at(origin).count--;
+    instance_counts.at(origin).destructs.insert( GetOrigin() );
+}
 
-//////////////////////////// free functions ///////////////////////////////
+
+void LeakCheck::Construct()
+{
+    if( instance_counts.count(origin)==0 )
+        instance_counts[origin] = {0};
+    instance_counts.at(origin).count++;
+}
+
+
+void LeakCheck::DumpCounts( int min )
+{
+    FTRACEC("LeakCheck:\n");
+    for( auto p : instance_counts )  
+        if( p.second.count >= min )
+            FTRACEC(p.first)
+                   (" %llu instances\n", p.second.count)
+                   ("destructs: ")(p.second.destructs)("\n");
+}
+
+#define TRY(SEQ, LEVEL) \
+    if( !ON_STACK( __builtin_frame_address(LEVEL) ) ) \
+        break; \
+    SEQ.push_back( __builtin_extract_return_addr(__builtin_return_address(LEVEL)) ); 
+ 
+
+LeakCheck::Origin LeakCheck::GetOrigin()
+{
+    Origin o;
+    do 
+    {
+        TRY(o, 1)
+        TRY(o, 2)
+        TRY(o, 3)
+        TRY(o, 4)
+        TRY(o, 5)
+    } while(0);
+    return o;
+}
+
+#undef TRY
+
+
+map<LeakCheck::Origin, LeakCheck::Block> LeakCheck::instance_counts;
+
 
 void DumpCounts( int min = 0 ) // for GCC
 {
     FTRACEC("Dumping counts...\n");
     LeakCheck::DumpCounts(min);
 }
+
+
--- src/common/serial.hpp
+++ src/common/serial.hpp
@@ -9,21 +9,88 @@
 #include <vector>
 #include <map>
 #include <set>
+#include <memory>
 
 using namespace std;
 
-
-struct PairHash
+//////////////////////////// SerialNumber ///////////////////////////////
+//
+// The idea is to provide an alternative to raw pointers for ordering our sets, miltsets etc.
+// This should be more repeatable - i.e. a slight disturbance to the dynamic memory allocator
+// should not cause everything to come out in a different order.
+//
+// We construct the new ordering as follows:
+// 1st: the step the object was constructed during (as specified by the top level)
+// 2nd: the location in the code where the object was constructed, foced down into 
+//      a sequential ordering to protect against changes in the location of code
+// 3rd: the count of objects constructed at that point.
+//
+// Note the 2nd criterion should differentiate between different object types, so 
+// no further action needed there.
+//
+class SerialNumber
 {
-    template<class T1, class T2>
-    size_t operator()( const pair<T1,T2> &p ) const 
+public:
+    typedef uint64_t SNType;
+    struct Hook
     {
-        auto h1 = hash<T1>{}(p.first);
-        auto h2 = hash<T2>{}(p.second);
-        return h1 ^ h2;  
-    }
+		virtual ~Hook() {};
+	};
+    
+protected:
+    SerialNumber();
+    SerialNumber( const SerialNumber &other );
+    SerialNumber &operator=( const SerialNumber &other );
+
+public:
+    bool operator<( const SerialNumber &o ) const;
+    inline SNType GetSerialNumber() const;
+    string GetSerialString() const; 
+	void SetHook(shared_ptr<Hook> h) const;
+	bool HasHook() const;
+	shared_ptr<Hook> GetHook() const;
+	
+private:    
+    SNType serial;
+    Progress progress;
+
+    static struct Cache
+    {
+        ~Cache();
+        map<Progress, SNType> master_serial_by_progress;        
+    } cache;
+    
+    mutable shared_ptr<Hook> hook;
 };
 
+//////////////////////////// SatelliteSerial ///////////////////////////////
+
+class SatelliteSerial
+{
+public:
+    typedef int SatelliteSNType;
+    
+    SatelliteSerial();
+    explicit SatelliteSerial( const SerialNumber *mother, const void *satellite );
+
+    string GetSerialString() const;
+    bool operator<( const SatelliteSerial &other ) const;
+    void Redetermine( const SerialNumber *mother, const void *satellite );
+    
+private:
+    static SatelliteSNType DetermineSerialNumber( const SerialNumber *mother, const void *satellite );
+
+	// These are hooked to the mother SerialNumber instance
+    struct MotherBlock : SerialNumber::Hook
+    {
+        map<const void *, SatelliteSNType> serial_by_satellite;
+        int next_serial=0;
+    };  
+
+    SatelliteSNType serial;
+};
+
+//////////////////////////// LeakCheck ///////////////////////////////
 
 class LeakCheck
 {
@@ -60,92 +127,5 @@ private:
     static map<Origin, Block> instance_counts;
 };
 
-//
-// The idea is to provide an alternative to raw pointers for ordering our sets, miltsets etc.
-// This should be more repeatable - i.e. a slight disturbance to the dynamic memory allocator
-// should not cause everything to come out in a different order.
-//
-// We construct the new ordering as follows:
-// 1st: the step the object was constructed during (as specified by the top level)
-// 2nd: the location in the code where the object was constructed, foced down into 
-//      a sequential ordering to protect against changes in the location of code
-// 3rd: the count of objects constructed at that point.
-//
-// Note the 2nd criterion should differentiate between different object types, so 
-// no further action needed there.
-//
-class SerialNumber
-{
-public:
-    typedef uint64_t SNType;
-    
-protected:
-    SerialNumber();
-    inline SerialNumber( const SerialNumber &other ) :
-        SerialNumber() // Identity semantics: ignore "other"
-    {      
-    }
-    inline SerialNumber &operator=( const SerialNumber &other )
-    {
-        // Identity semantics: ignore "other"
-        return *this;
-    }
-
-public:
-    bool operator<( const SerialNumber &o ) const
-    {
-        if( progress != o.progress )
-            return progress < o.progress;
-        else
-            return serial < o.serial;
-    }
-    
-    inline SNType GetSerialNumber() const 
-    {
-        return serial; // This is enough for uniqueness
-    }
-    string GetSerialString() const; 
-    
-private:    
-    SNType serial;
-    Progress progress;
-
-    static struct Cache
-    {
-        ~Cache();
-
-        map<Progress, SNType> master_serial_by_progress;
-    } cache;
-};
-
-
-class SatelliteSerial
-{
-public:
-    typedef int SatelliteSNType;
-    
-    SatelliteSerial();
-    explicit SatelliteSerial( const SerialNumber *mother, const void *satellite );
-
-    string GetSerialString() const;
-    bool operator<( const SatelliteSerial &other ) const;
-    void Redetermine( const SerialNumber *mother, const void *satellite );
-    
-private:
-    static SatelliteSNType DetermineSerialNumber( const SerialNumber *mother, const void *satellite );
-
-    struct MotherBlock
-    {
-        map<const void *, SatelliteSNType> serial_by_satellite;
-        int next_serial=0;
-    };
-    // Index by location, then serial. Serial scales with input tree size so
-    // use unordered_map for that.
-    typedef unordered_map< SerialNumber::SNType, 
-                           MotherBlock > BlocksByMotherSerial;
-    static BlocksByMotherSerial blocks_by_mother_serial;    
-
-    SatelliteSNType serial;
-};
 
 #endif
