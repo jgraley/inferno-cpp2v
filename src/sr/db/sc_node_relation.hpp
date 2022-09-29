13d96e5fc2a865277e588bcd53cd97e04b6098d4
John <jgraley1@gmail.com>
2022-09-29 17:46:09 +0100

    start new relation for #647

---
+++ src/sr/db/sc_node_relation.hpp
@@ -0,0 +1,37 @@
+#ifndef SC_NODE_RELATION_HPP
+#define SC_NODE_RELATION_HPP
+
+#include "common/common.hpp"
+#include "node/specialise_oostd.hpp"
+#include "helpers/transformation.hpp"
+#include "helpers/walk.hpp"
+#include "helpers/flatten.hpp"
+#include "link.hpp"
+
+#include <memory>
+
+class SimpleCompare;
+
+namespace SR
+{
+class SimpleCompareNodeRelation
+{
+public:
+    SimpleCompareNodeRelation();
+    Orderable::Result Compare( XLink xlink, XLink ylink ) const;
+
+    /// Less operator: for use with set, map etc
+    bool operator()( XLink xlink, XLink ylink ) const;
+    
+    /// Perform a test to check the reflexive, symmetric and 
+    /// transitive properties, given a representative set
+    /// of links. Pass if no assert failure.
+    void TestProperties( const unordered_set<XLink> &xlinks ) const;
+    
+private:
+    shared_ptr<SimpleCompare> simple_compare;
+}; 
+
+};
+
+#endif
