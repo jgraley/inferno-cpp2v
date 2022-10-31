d9166e9e3d16666d0c9facbacf9509e5f4f73429
John <jgraley1@gmail.com>
2022-10-31 17:51:53 +0000

    fix #677

--- test/examples/issue677.cpp
+++ test/examples/issue677.cpp
@@ -1,7 +1,22 @@
-int main()
+#include <isystemc.h>
+
+class TopLevel : public sc_module
 {
-    int a[2];
-    // TypeOf(&(a[1])) is pointer not appearing anywhere else.
-    // It points to int, which does exist, in the line above.
-	return &(a[1]) - &(a[0]);
-}
+public:
+    int x;
+    SC_CTOR(TopLevel)
+    {
+        SC_THREAD(T);        
+    }
+    void T()
+    {
+        int a[2];
+        // TypeOf(&(a[1])) is pointer not appearing anywhere else.
+        // It points to int, which does exist, in the line above.
+        int r = &(a[1]) - &(a[0]);
+        cease( r );
+    }
+};
+
+TopLevel top_level("top_level");
+
