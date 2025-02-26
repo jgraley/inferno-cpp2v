b1f6f73ba5fa891e0f3ffd0a06402b5b20ca6157
John Graley <jgraley1@gmail.com>
2025-02-26 18:46:14 +0000

    safety commit

--- src/sr/up/tz_ordering.cpp
+++ src/sr/up/tz_ordering.cpp
@@ -73,10 +73,12 @@ void TreeZoneOrderingHandler::RunForRange( shared_ptr<ZoneExpression> &base,
 	ZoneExprPtrList tree_zone_op_list;
 	InsertTZsBypassingFZs( base, tree_zone_op_list, tree_zone_op_list.end() );
 	
+	ZoneExprPtrList::iterator next_it;
 	for( ZoneExprPtrList::iterator it = tree_zone_op_list.begin();
 	     it != tree_zone_op_list.end();
-	     ++it )
+	     it = next_it )
 	{
+		next_it = next(it);
 		shared_ptr<ZoneExpression> *expr = *it;
 		auto ptz_op = dynamic_pointer_cast<DupMergeTreeZoneOperator>(*expr);
 		ASSERT( ptz_op ); // should succeed due InsertTZsBypassingFZs()
@@ -94,27 +96,27 @@ void TreeZoneOrderingHandler::RunForRange( shared_ptr<ZoneExpression> &base,
 				FTRACE(db->GetOrderings().depth_first_ordering)("\n");
 				ASSERT(diff_begin >= 0)("Tree zone base ")(tz_base)(" appears before limit ")(range_first)(" in X tree");
 				ASSERT(diff_end <= 0)("Tree zone base ")(tz_base)(" appears after limit ")(range_last)(" in X tree");
+				ASSERTFAIL(); // we aint goin nowhere
 			}
-			else
+
+			// The current TZ op is in the wrong place. It will be turned into
+			// a free zone. Free zones are "invisible" in this scheme, so we update
+			// our current work list to include its children (bypassing FZs).
+			ptz_op->ForChildren( [&](shared_ptr<ZoneExpression> &child_expr)
 			{
-				// The current TZ op is in the wrong place. It will be turned into
-				// a free zone. Free zones are "invisible" in this scheme, so we update
-				// our current work list to include its children (bypassing FZs).
-				ZoneExprPtrList::iterator next_it = next(it);
-				ptz_op->ForChildren( [&](shared_ptr<ZoneExpression> &child_expr)
-				{
-					InsertTZsBypassingFZs( child_expr, tree_zone_op_list, next_it );
-				} );
-				
-				// Duplicate the TZ that's in the wrong place into a free zone. 
-				// Hopefully because we already dealt with children, we could
-				// just mark this in side data.
-				TRACE("Out of sequence: duplicating ")(tz_base)("\n");
-				out_of_order_list.push_back(expr);
-				
-				continue; // no range threshold update or recurse
-			}
+				InsertTZsBypassingFZs( child_expr, tree_zone_op_list, next_it );
+			} );
+			next_it = tree_zone_op_list.erase(it);
+			
+			// Duplicate the TZ that's in the wrong place into a free zone. 
+			// Hopefully because we already dealt with children, we could
+			// just mark this in side data.
+			TRACE("Out of sequence: duplicating ")(tz_base)("\n");
+			out_of_order_list.push_back(expr);
+			
+			continue; // no range threshold update or recurse
 		}
+				
 		TRACE("OK, updating start of range\n");
 		
 	    // Narrow the acceptable range for the next tree zone
