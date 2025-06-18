f7b8aa66ac9c3ea8822d21eacf92f39e6e5d6d16
John <jgraley1@gmail.com>
2025-06-18 19:27:55 +0100

    always cache using unordere_map

--- src/sr/db/lacing.hpp
+++ src/sr/db/lacing.hpp
@@ -32,7 +32,18 @@ class Lacing
 {
 public:    
     typedef set<TreePtr<Node>> CategorySet;
-    typedef map<pair<type_index, type_index>, bool> SubCategoryCache;
+	struct pair_hash {
+		template <class T1, class T2>
+		std::size_t operator () (const std::pair<T1,T2> &p) const {
+			auto h1 = std::hash<T1>{}(p.first);
+			auto h2 = std::hash<T2>{}(p.second);
+
+			// Mainly for demonstration purposes, i.e. works but is overly simple
+			// In the real world, use sth. like boost.hash_combine
+			return h1 ^ h2;  
+		}
+	};
+    typedef unordered_map<pair<type_index, type_index>, bool, pair_hash> SubCategoryCache;
 
     Lacing();
 
