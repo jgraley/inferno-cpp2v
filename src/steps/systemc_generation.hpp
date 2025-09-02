2191dbc537e994021975d47f1df4e3caa13da3ae
John <jgraley1@gmail.com>
2025-09-02 19:24:49 +0100

    oops again

---
+++ src/steps/systemc_generation.hpp
@@ -0,0 +1,44 @@
+#ifndef SYSTEMC_GENERATION
+#define SYSTEMC_GENERATION
+
+// These transformations convert C code into SystemC.
+
+#include "tree/cpptree.hpp"
+#include "tree/sctree.hpp"
+#include "sr/vn_step.hpp"
+
+namespace Steps {
+
+using namespace SR;
+
+class GlobalScopeToModule : public VNStep 
+{
+public:
+    GlobalScopeToModule();
+};
+
+
+class MainToThread : public VNStep 
+{
+public:
+    MainToThread();
+};
+
+
+/** 
+ * Builder for the steps that generate SystemC from C code 
+ * 
+ * We will only act if no SystemC modules are detected. That tells us
+ * that the input program is not SC, so must be C. We create
+ * an SC module called GlobalScope and move the free functions
+ * (including main()) into it.
+ * */
+class GenerateSC
+{
+public:
+    static void Build( vector< shared_ptr<VNStep> > *sequence );
+};
+
+};
+
+#endif
