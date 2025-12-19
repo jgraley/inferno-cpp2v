3a8df1128df92f1ed0cd556c37f7a774b4ceff7f
John Graley <jgraley1@gmail.com>
2025-12-19 17:12:20 +0000

    finally sort out logging in tests and add a full suite to parser_check

--- parser_check.sh
+++ parser_check.sh
@@ -12,13 +12,12 @@ mkdir -p p2
 rm -f p2/*
 ./inferno.exe p1 -p p2/ -qX
 
-#diff --color p1 p2
-
+# Smoke tests: for one test ccase and one quit-after, compare the transformed outputs
 TC=small.c
 QA=T130+
-
 ./inferno.exe p1 -i test/examples/${TC} -q${QA} > out_p1.cpp
 ./inferno.exe    -i test/examples/${TC} -q${QA} > out_builtin.cpp
-diff --color out_p1.cpp out_builtin.cpp
+diff --color out_builtin.cpp out_p1.cpp
 
-test/exec_test.sh test/examples/${TC} test/resultspb/exec "p1 -q${QA}"
+# Test run using patterns we previously wrote to p1/
+make -j 30 test I="p1"
