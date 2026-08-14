336c8c9a72fac79f1b93e9d848eef0ffd6d80868
John Graley <jgraley1@gmail.com>
2026-08-14 15:52:30 +0100

    dep trimming

--- src/vn/lang/vn_actions.hpp
+++ src/vn/lang/vn_actions.hpp
@@ -1,8 +1,8 @@
 #ifndef VN_PARSE_HPP
 #define VN_PARSE_HPP
 
-#include "tree/cpptree.hpp"
-#include "tree/sctree.hpp"
+//#include "tree/cpptree.hpp"
+//#include "tree/sctree.hpp"
 #include "helpers/transformation.hpp"
 #include "uniquify_identifiers.hpp"
 #include "helpers/simple_compare.hpp"
