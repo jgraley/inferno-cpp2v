9a81287d2e7ddd5c16bd9dd296be8d7b4dd4cac5
John Graley <jgraley1@gmail.com>
2026-09-06 18:12:31 +0100

    safety

--- src/tree/localtree.hpp
+++ src/tree/localtree.hpp
@@ -39,34 +39,25 @@ namespace LocalTree {
 // From Simple C to SC
 struct GlobalsModule : SCTree::Module { NODE_FUNCTIONS_FINAL };
 
-// From Lower Control Flow 
-// Local nodes let us designate switch and for nodes as uncombable
+// From LowerControlFlow 
+// These work with short-form explicits
 struct UncombableSwitch : CPPTree::Switch, CPPTree::Uncombable { NODE_FUNCTIONS_FINAL KEYWORD_AS_BASE_IN_CPP_ONLY(CPPTree::Switch) };
 struct UncombableFor : CPPTree::For, CPPTree::Uncombable { NODE_FUNCTIONS_FINAL KEYWORD_AS_BASE_IN_CPP_ONLY(CPPTree::For) };
 struct CombableFor : CPPTree::For { NODE_FUNCTIONS_FINAL KEYWORD_AS_BASE_IN_CPP_ONLY(CPPTree::For) };
 struct UncombableBreak : CPPTree::Break, CPPTree::Uncombable { NODE_FUNCTIONS_FINAL KEYWORD_AS_BASE_IN_CPP_ONLY(CPPTree::Break) };
 struct CombableBreak : CPPTree::Break { NODE_FUNCTIONS_FINAL KEYWORD_AS_BASE_IN_CPP_ONLY(CPPTree::Break) };
 
-// From Generate stacks
+// From GenerateStacks
+// Requires long-form explicit because no keyword to override
 struct TempReturnAddress : CPPTree::Temporary { NODE_FUNCTIONS_FINAL RENDER_AS_BASE_IN_CPP_ONLY(CPPTree::Temporary) };
 
 // From Fall Out
-// A label with a piggybacked pointer to the corresponding enum value
+// Requires long-form explicit because no keyword to override and no syntax for specifying state
 struct StateLabel : CPPTree::LabelDeclaration
 {
     NODE_FUNCTIONS_FINAL 
     RENDER_AS_BASE_IN_CPP_ONLY(CPPTree::LabelDeclaration)
     TreePtr<CPPTree::InstanceIdentifier> state;
-    
-	TreePtr<Node> OnArgsList( list<TreePtr<Node>> args, YY::VNLangParser::location_type loc ) override
-	{
-		if( args.size() != 1 )
-			throw YY::VNLangParser::syntax_error(
-				any_cast<YY::VNLangParser::location_type>(loc),
-				MyBestErrName() + " requires one argument.");
-		state = SoloElementOf(args);
-		return (TreePtr<Node>)shared_from_this();
-	}
 };
 
 
